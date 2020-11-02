0f0aaa1ccbc4036c032ac574064748827b894c36
adriencorsetti <72150968+adriencorsetti@users.noreply.github.com>
2020-11-02 17:16:31 +0100

    Modification A 02/11/20

--- js/Reservation.js
+++ js/Reservation.js
@@ -1,1 +1,36 @@
+class Destination{
+    constructor(ville,petitdej,prix,animaux){
+        this._ville = ville;
+        this._petitdej = petitdej;
+        this._prix = prix;
+        this._animaux = animaux;
+    }
+}
+
+tabDest = [
+    new Destination("Marseille",true,500,false),
+    new Destination("Paris",true,500,false),
+    new Destination("Barcelone",true,500,false),
+    new Destination("Madrid",true,500,false),
+    new Destination("Los-Angeles",true,500,false),
+    new Destination("Cancun",true,500,false),
+    new Destination("Lima",true,500,false),
+    new Destination("Rio-de-Janeiro",true,500,false),
+    new Destination("Londres",true,500,false),
+    new Destination("Shanghai",true,500,false),
+    new Destination("Phuket",true,500,false),
+    new Destination("Johannesburg",true,500,false),
+    new Destination("Rome",true,500,false),
+    new Destination("Kuta",true,500,false),
+    new Destination("Bora-bora",true,500,false)
+]
+
+
+
 let sejour_id = new URLSearchParams(window.location.search).get("id")
+
+const tabDest
+
+const found = tabDest.find(element => element == sejour_id);
+
+console.log(found);
