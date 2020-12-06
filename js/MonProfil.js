02e96e101a1c962be778b4cb86cb6ea68fdeb8c3
Adrien Corsetti <adrien.corsetti@cpe.fr>
2020-12-06 15:08:18 +0100

    A1 06/12/2020

---
+++ js/MonProfil.js
@@ -0,0 +1,17 @@
+
+var myInput = document.getElementById("psw");
+var usrname = document.getElementById("usrname");
+
+
+
+// Fonction qui valide l'authentification
+    if(usrname in ["totorina13","maradona10"]) {
+        remove("valid")
+    } else {
+        remove("invalid")
+
+}
+
+
+
+
