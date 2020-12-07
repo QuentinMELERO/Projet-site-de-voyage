731659893ed309bac0423117de25be7f21a930a4
QuentinMELERO <Quentin.melero@hotmail.fr>
2020-12-07 09:03:19 +0100

    Q 1 07/12/2020

--- js/Destination.js
+++ js/Destination.js
@@ -35,15 +35,27 @@ for ( const v of tabDest){
     .replace(/{{ville}}/g, v._ville)
     .replace(/{{prix}}/g, v._prix)
     .replace(/{{id}}/g, tabDest.indexOf(v));
+    Temperature(v._ville)
 
 clone.firstElementChild.innerHTML = newContent
 
 document.querySelector(".tableau").appendChild(clone);
 }
 
+function Temperature(){
+  let appid = "931f79e58856d6da0aadd4909cc15594"
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + "{{ville}}" + "&appid=" + appid + "&units=metric")
+  .then(function(response) {
+      return response.json();
+  })
+  .then(function(json) {
+      document.querySelector("#{{ville}}").innerHTML="La température de " + json["name"] + " est de " + json["main"]["temp"] + "°C"
+  })
+}
 
 
-// When the user scrolls the page, execute myFunction 
+
+// Quand l'utilisateur scroll la page, execute myFunction 
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
--- js/Temperature.js
+++ js/Temperature.js
@@ -1,10 +1,10 @@
-function Temperature() {​​​​
+function Temperature(){
     let appid = "931f79e58856d6da0aadd4909cc15594"
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + "Paris" + "&appid=" + appid + "&units=metric")
-    .then(function(response) {​​​​
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + "{{ville" + "&appid=" + appid + "&units=metric")
+    .then(function(response) {
         return response.json();
-    }​​​​)
-    .then(function(json) {​​​​
-        document.querySelector("#​​​​Paris").innerHTML="La température de " + json["name"] + " est de " + json["main"]["temp"] + "°C"
-    }​​​​)
-  }
+    })
+    .then(function(json) {
+        document.querySelector("#").innerHTML="La température de " + json["name"] + " est de " + json["main"]["temp"] + "°C"
+    })
+}
