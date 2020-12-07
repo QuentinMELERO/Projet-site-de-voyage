3020b25f52b86ce071f685de1455121e14c7cbe1
QuentinMELERO <Quentin.melero@hotmail.fr>
2020-12-07 16:20:04 +0100

    Q 4 07/12/2020

--- js/Destination.js
+++ js/Destination.js
@@ -1,59 +1,62 @@
 class Destination{
-    constructor(ville,petitdej,prix,animaux,continent){
+  constructor(ville,petitdej,prix,animaux,continent,villeA,temperature){
         this._ville = ville;
         this._petitdej = petitdej;
         this._prix = prix;
         this._animaux = animaux;
         this._continent = continent;
+        this._villeA = villeA
+        this._temperature = temperature;
     }
 }
 
 tabDest = [
-  new Destination("Marseille",true,100,false,"Europe"),
-  new Destination("Paris",true,150,false,"Europe"),
-  new Destination("Barcelone",true,150,false,"Europe"),
-  new Destination("Madrid",true,150,false,"Europe"),
-  new Destination("Los-Angeles",true,300,false,"Amerique"),
-  new Destination("Cancun",true,350,false,"Amerique"),
-  new Destination("Lima",true,350,false,"Amerique"),
-  new Destination("Rio-de-Janeiro",true,350,false,"Amerique"),
-  new Destination("Londres",true,200,false,"Europe"),
-  new Destination("Shanghai",true,400,false,"Asie"),
-  new Destination("Phuket",true,250,false,"Asie"),
-  new Destination("Johannesburg",true,400,false,"Afrique"),
-  new Destination("Rome",true,150,false,"Europe"),
-  new Destination("Kuta",true,400,false,"Asie"),
-  new Destination("Bora-bora",true,500,false,"Asie")
+  new Destination("Marseille",true,100,false,"Europe","Marseille"),
+  new Destination("Paris",true,150,false,"Europe","Paris"),
+  new Destination("Barcelone",true,150,false,"Europe","Barcelone"),
+  new Destination("Madrid",true,150,false,"Europe","Madrid"),
+  new Destination("Los-Angeles",true,300,false,"Amerique","Los Angeles"),
+  new Destination("Cancun",true,350,false,"Amerique","Cancun"),
+  new Destination("Lima",true,350,false,"Amerique","Lima"),
+  new Destination("Rio-de-Janeiro",true,350,false,"Amerique","Rio de Janeiro"),
+  new Destination("Londres",true,200,false,"Europe","Londres"),
+  new Destination("Shanghai",true,400,false,"Asie","Shanghai"),
+  new Destination("Phuket",true,250,false,"Asie","Phuket"),
+  new Destination("Johannesburg",true,400,false,"Afrique","Johannesburg"),
+  new Destination("Rome",true,150,false,"Europe","Rome"),
+  new Destination("Kuta",true,400,false,"Asie","Kuta"),
+  new Destination("Bora-bora",true,500,false,"Asie","French polynesia")
 ]
 
+tabDestpromesse=tabDest.map(destination=>Temperature(destination))
+Promise.all(tabDestpromesse).then(tabDesttemp=>{
 
 let template = document.querySelector("#listeDestination");
-for ( const v of tabDest){
+for ( const v of tabDesttemp){
     let clone = document.importNode(template.content,true);
 
     newContent = clone.firstElementChild.innerHTML
     .replace(/{{ville}}/g, v._ville)
     .replace(/{{prix}}/g, v._prix)
-    .replace(/{{id}}/g, tabDest.indexOf(v));
-    Temperature(v._ville)
+    .replace(/{{id}}/g, tabDest.indexOf(v))
+    .replace(/{{temperature}}/g, v._temperature);
 
 clone.firstElementChild.innerHTML = newContent
 
 document.querySelector(".tableau").appendChild(clone);
 }
-
-function Temperature(){
+})
+  
+  function Temperature(destination){
   let appid = "931f79e58856d6da0aadd4909cc15594"
-  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + "{{ville}}" + "&appid=" + appid + "&units=metric")
+  return fetch("https://api.openweathermap.org/data/2.5/weather?q=" + destination._villeA + "&appid=" + appid + "&units=metric")
   .then(function(response) {
-      return response.json();
+      return response.json()
   })
-  .then(function(json) {
-      document.querySelector("#{{ville}}").innerHTML="La température de " + json["name"] + " est de " + json["main"]["temp"] + "°C"
-  })
+  .then(function(json){
+      return new Destination(destination._ville,destination._petitdej,destination._prix,destination._animaux,destination._continent,destination._villeA,json.main.temp);
+  }).catch(function(){return destination})
 }
-
-
 
 // Quand l'utilisateur scroll la page, execute myFunction 
 window.onscroll = function() {myFunction()};
--- js/Recapinfos.js
+++ js/Recapinfos.js
@@ -1,3 +1,37 @@
+class Destination{
+    constructor(ville,petitdej,prix,animaux,continent){
+        this._ville = ville;
+        this._petitdej = petitdej;
+        this._prix = prix;
+        this._animaux = animaux;
+        this._continent = continent;
+    }
+}
+
+tabDest = [
+    new Destination("Marseille",true,100,false,"Europe"),
+    new Destination("Paris",true,150,false,"Europe"),
+    new Destination("Barcelone",true,150,false,"Europe"),
+    new Destination("Madrid",true,150,false,"Europe"),
+    new Destination("Los-Angeles",true,300,false,"Amerique"),
+    new Destination("Cancun",true,350,false,"Amerique"),
+    new Destination("Lima",true,350,false,"Amerique"),
+    new Destination("Rio-de-Janeiro",true,350,false,"Amerique"),
+    new Destination("Londres",true,200,false,"Europe"),
+    new Destination("Shanghai",true,400,false,"Asie"),
+    new Destination("Phuket",true,250,false,"Asie"),
+    new Destination("Johannesburg",true,400,false,"Afrique"),
+    new Destination("Rome",true,150,false,"Europe"),
+    new Destination("Kuta",true,400,false,"Asie"),
+    new Destination("Bora-bora",true,500,false,"Asie")
+]
+
+
+let sejour_id = new URLSearchParams(window.location.search).get("id");
+document.getElementById("titre").textContent="Votre réservation pour " + tabDest[sejour_id]._ville;
+document.getElementById("photo").setAttribute("src","../image/" + tabDest[sejour_id]._ville + ".jpg")
+
+
 function recap(){
 
     let nom = new URLSearchParams(window.location.search).get("Nom");
@@ -41,3 +75,7 @@ function random(){
     let num = Math.floor(10000*Math.random());
     document.getElementById("numresa").textContent="Réservation n°" + num;
 }
+
+let prenomfin = new URLSearchParams(window.location.search).get("prenomrecap");
+let nomfin = new URLSearchParams(window.location.search).get("nomrecap");
+document.getElementById("finresa").textContent="Bonjour," + prenomfin + nomfin;
